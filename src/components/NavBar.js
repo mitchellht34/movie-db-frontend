c01376587d46d53f00cf8750be93681ef6fe522b
Mitch Toran <mitchellht34@gmail.com>
2024-06-23 21:02:34 -0400

    add navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,36 @@
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+
+    return (
+      <div>
+        <NavLink
+          to="/"
+          exact
+          style={{ marginRight: "10px" }}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/search"
+          style={{ marginRight: "10px" }}
+        >
+          Search
+        </NavLink>
+        <NavLink
+          to="/favorites"
+          style={{ marginRight: "10px" }}
+        >
+          My Favorites
+        </NavLink>
+        <NavLink
+          to="/new-entry"
+          style={{ marginRight: "10px" }}
+        >
+          Manual Entry
+        </NavLink>
+      </div>
+    );
+  }
+  
+  export default NavBar;
